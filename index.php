18b711bf01ef4be871fa5d27d3abbd8686f5c743
Pedro Serquiz <pedroserquiz@gmail.com>
2014-05-19 22:47:24 -0300

    Topo e rodapé criados

--- index.php
+++ index.php
@@ -1,1 +1,111 @@
-teste
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Jumbotron Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+  <body>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+        </div><!--col-md-3-->
+
+        <div class="col-md-3">
+          <div id="logo">
+          </div>
+        </div><!--col-md-3-->
+
+        <div class="col-md-3">
+          <div id="caixa-foto">
+            <div id="foto">
+            </div>
+          </div>
+          <span id="texto-topo">
+          <p>
+          Luiza Azevedo & Duda Almeida
+          (84) 9981-0828 | (84) 9407-8949
+          CONTATO@LDARQUITETURA.ARQ.BR</p>
+        </span>
+        </div><!--col-md-3-->
+
+        <div class="col-md-3">
+        </div><!--col-md-3-->
+      </div><!--row-->
+    </div>
+
+    <hr class="hr-dourado">
+
+    
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-12">
+          <div class="col-md-3">
+        </div><!--col-md-3-->
+          <div class="col-md-6">
+            <form>
+                    <div class="input-group input-group-lg">
+                      <input type="text" class="form-control" placeholder="Username">
+                    </div>
+
+
+            </form>
+          </div>
+        </div>
+      </div>
+
+
+    </div> <!-- /container -->
+          <hr class="hr-dourado-grosso">
+
+      <footer>
+        <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-12">
+          <div class="col-md-3">
+        </div><!--col-md-3-->
+          <div class="col-md-6">
+          <h2 class="titulo-instagram">@LD_Arquitetura</h2>
+          <!-- SnapWidget -->
+          <iframe src="http://snapwidget.com/in/?u=bGRfYXJxdWl0ZXR1cmF8aW58MTQ4fDR8MXx8eWVzfDV8ZmFkZUlufG9uU3RhcnR8eWVz&v=19514" title="Instagram Widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:652px; height:163px"></iframe>
+          </div>
+        </div>
+      </div>
+
+
+    </div> <!-- /container -->
+      </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
